87f412bfa9d7cba6d0593cbe431b258952c4308e
William Palin <flooie@users.noreply.github.com>
2019-07-05 13:43:32 -0400

    Update solr@4.10.4.rb

--- solr@4.10.4.rb
+++ solr@4.10.4.rb
@@ -3,7 +3,8 @@ class SolrAT4104 < Formula
   homepage "https://lucene.apache.org/solr/"
   url "https://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.10.4/solr-4.10.4.tgz"
   mirror "https://archive.apache.org/dist/lucene/solr/4.10.4/solr-4.10.4.tgz"
-
+  sha256 "ac3543880f1b591bcaa962d7508b528d7b42e2b5548386197940b704629ae851"
+  
   bottle :unneeded
 
   keg_only :versioned_formula
